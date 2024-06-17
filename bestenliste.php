5ae2cbb275e4eba64886d1341c1f2e6ff4dd4272
masn <167069469+masnlp@users.noreply.github.com>
2024-06-17 02:15:58 +0200

    Update bestenliste.php yay

--- bestenliste.php
+++ bestenliste.php
@@ -5,9 +5,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Elias Schinkinger">
     <title>Bestenliste</title>
+    <link rel="stylesheet" href="stylebest.css">
 </head>
 <body>
-    
+<header id="header">
+        <div id="heading">
+            <h1>RateTheFood</h1>
+           
+
+            <div id="loggin">
+            <?php 
+
+            if(isset($_SESSION["user"]))
+            {
+                $user = $_SESSION["user"]["Username"];
+
+                echo "<a href='logout.php'>SIGN OUT</a>";
+                echo "<a href='index.php'>HOME</a>";
+            } else {
+                echo "<a href='login.php' style='margin-right: 10px;'>LOG IN </a>";
+                echo "<a href='signup.php'>SIGN UP</a>";
+                
+            }
+
+            ?>
+            </div>
+
+            <div id="links">
+                <a href="index.php" style="margin-right: 10px;">GERICHTE</a>
+                <a href="bestenliste.php" style="margin-right: 10px;">BESTENLISTE</a>
+                <a href="#">IMPRESSUM</a>
+            </div>
+        </div>
+
+        <div id="bar">
+            
+        </div>
+    </header>
+
+
     <?php
     
     $db = mysqli_connect("eliasschinkinger.lima-db.de:3306", "USER436891_gaudi", "gAudI420!?", "db_436891_2");
@@ -22,14 +58,35 @@
         $pathToPic = $row['PachToPic'];
         $likes     = $row[  'Likes'  ];
 
-        echo $foodName. " ". " from: ". $row['Nationality'];
+        
+
+        echo "<table>
+            
+            <tr>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+            </tr>
+            <tr class='hmp_foods'>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+            </tr>
 
 
-        echo '<div>';
-        echo "<img src=\"$pathToPic\" alt=\"Pic of $foodName\" style=\"width: 100px; height: 100px;\">";
-        echo "Likes: ".$likes;
-        echo '</div>';
+            <tr>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+                <td class='text_in_table'><div id='hmp_food_descrip'>$foodName: $likes Like/s </div><br></td>
+                <td><img src=\"$pathToPic\" alt=\"Pic of $foodName\" style=\"width: 400px;\"></td>
+                <td class='hmp_empty'>Dies ist ein Platzhaltertext.............</td>
+                
+            </tr>
 
+            
+            
+        </table>";
     }
 
     mysqli_close($db);
